384c36c6246eee76aa5aedbb34700559ca4f4f9b
KokiHirokawa <kitakitsune1110@gmail.com>
2024-12-18 00:53:13 +0900

    LayoutView

--- saba_core/src/renderer/dom/node.rs
+++ saba_core/src/renderer/dom/node.rs
@@ -174,6 +174,20 @@ impl Element {
     pub fn kind(&self) -> ElementKind {
         self.kind
     }
+
+    pub fn is_block_element(&self) -> bool {
+        match self.kind {
+            ElementKind::Body
+            | ElementKind::H1
+            | ElementKind::H2
+            | ElementKind::P => true,
+            _ => false,
+        }
+    }
+
+    pub fn attributes(&self) -> Vec<Attribute> {
+        self.attributes.clone()
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
--- saba_core/src/renderer/layout/layout_view.rs
+++ saba_core/src/renderer/layout/layout_view.rs
@@ -3,8 +3,10 @@ use core::cell::RefCell;
 use crate::renderer::dom::node::ElementKind;
 use crate::renderer::dom::node::Node;
 use alloc::rc::Rc;
-
+use crate::constants::CONTENT_AREA_WIDTH;
 use crate::renderer::css::cssom::StyleSheet;
+use crate::renderer::dom::api::get_target_element_node;
+use crate::renderer::layout::layout_object::{create_layout_object, LayoutObject, LayoutObjectKind, LayoutPoint, LayoutSize};
 
 #[derive(Debug, Clone)]
 pub struct LayoutView {
@@ -25,4 +27,110 @@ impl LayoutView {
     pub fn root(&self) -> Option<Rc<RefCell<LayoutObject>>> {
         self.root.clone()
     }
+}
+
+impl LayoutView {
+    fn update_layout(&mut self) {
+        Self::calculate_node_size(&self.root, LayoutSize::new(CONTENT_AREA_WIDTH, 0));
+
+        Self::calculate_node_position(
+            &self.root,
+            LayoutPoint::new(0, 0),
+            LayoutObjectKind::Block,
+            None,
+            None,
+        );
+    }
+
+    fn calculate_node_size(node: &Option<Rc<RefCell<LayoutObject>>>, parent_size: LayoutSize) {
+        if let Some(n) = node {
+            if n.borrow().kind() == LayoutObjectKind::Block {
+                n.borrow_mut().compute_size(parent_size);
+            }
+
+            let first_child = n.borrow().first_child();
+            Self::calculate_node_size(&first_child, n.borrow().size());
+
+            let next_sibling = n.borrow().next_sibling();
+            Self::calculate_node_size(&next_sibling, parent_size);
+
+            n.borrow_mut().compute_size(parent_size);
+        }
+    }
+}
+
+fn build_layout_tree(
+    node: &Option<Rc<RefCell<Node>>>,
+    parent_obj: &Option<Rc<RefCell<LayoutObject>>>,
+    cssom: &StyleSheet,
+) -> Option<Rc<RefCell<LayoutObject>>> {
+    let mut target_node = node.clone();
+    let mut layout_object = create_layout_object(node, parent_obj, cssom);
+
+    while layout_object.is_none() {
+        if let Some(n) = target_node {
+            target_node = n.borrow().next_sibling().clone();
+            layout_object = create_layout_object(&target_node, parent_obj, cssom);
+        } else {
+            return layout_object;
+        }
+    }
+
+    if let Some(n) = target_node {
+        let original_first_child = n.borrow().first_child();
+        let original_next_sibling = n.borrow().next_sibling();
+        let mut first_child = build_layout_tree(&original_first_child, &layout_object, cssom);
+        let mut next_sibling = build_layout_tree(&original_next_sibling, &None, cssom);
+
+        if first_child.is_none() && original_first_child.is_some() {
+            let mut original_dome_node = original_first_child
+                .expect("first child should exist")
+                .borrow()
+                .next_sibling();
+
+            loop {
+                first_child = build_layout_tree(&original_dome_node, &layout_object, cssom);
+
+                if first_child.is_none() && original_dome_node.is_some() {
+                    original_dome_node = original_dome_node
+                        .expect("next sibling should exist")
+                        .borrow()
+                        .next_sibling();
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        if next_sibling.is_none() && original_next_sibling.is_some() {
+            let mut original_dome_node = original_next_sibling
+                .expect("first child should exist")
+                .borrow()
+                .next_sibling();
+
+            loop {
+                next_sibling = build_layout_tree(&original_dome_node, &None, cssom);
+
+                if next_sibling.is_none() && original_dome_node.is_some() {
+                    original_dome_node = original_dome_node
+                        .expect("next sibling should exist")
+                        .borrow()
+                        .next_sibling();
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        let obj = match layout_object {
+            Some(ref obj) => obj,
+            None => panic!("render object should exist here"),
+        };
+        obj.borrow_mut().set_first_child(first_child);
+        obj.borrow_mut().set_next_sibling(next_sibling);
+    }
+
+    layout_object
 }
